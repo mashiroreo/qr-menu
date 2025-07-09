2b5d473aeba5397afb4f72eaae90ec18df7e146d
mashiroreo <107978522+mashiroreo@users.noreply.github.com>
2025-07-09 11:30:24 +0900

    fix(infra): set Cloud SQL edition to ENTERPRISE and remove Cloud Run service from Terraform; Cloud Run will be deployed by Cloud Build pipeline

--- infra/artifact_run.tf
+++ infra/artifact_run.tf
@@ -3,43 +3,4 @@ resource "google_artifact_registry_repository" "qrmenu" {
   location      = "asia-northeast1"
   repository_id = "qrmenu"
   format        = "DOCKER"
-}
-
-resource "google_cloud_run_service" "api" {
-  name     = "qrmenu-api"
-  location = "asia-northeast1"
-
-  template {
-    spec {
-      service_account_name = "qrmenu-run-sa@${var.project_id}.iam.gserviceaccount.com"
-
-      containers {
-        image = "asia-northeast1-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.qrmenu.repository_id}/qrmenu-api:latest"
-
-        env {
-          name = "DATABASE_URL"
-          value_from {
-            secret_key_ref {
-              name = google_secret_manager_secret.database_url.secret_id
-              key  = "latest"
-            }
-          }
-        }
-      }
-    }
-  }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-
-  autogenerate_revision_name = true
-}
-
-resource "google_cloud_run_service_iam_member" "public_invoker" {
-  service  = google_cloud_run_service.api.name
-  location = google_cloud_run_service.api.location
-  role     = "roles/run.invoker"
-  member   = "allUsers"
 } 
